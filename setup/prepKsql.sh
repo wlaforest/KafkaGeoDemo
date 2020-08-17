114e0b8e47cee6dc7e923e383954ba906cf9abb2
Will LaForest <lacapaneus@gmail.com>
2020-08-17 10:07:38 -0400

    from cta to generic bus

--- setup/prepKsql.sh
+++ setup/prepKsql.sh
@@ -1,5 +1,5 @@
 ksql < setup/createGeoFence.sql
-ksql < setup/createStreamsCta.sql
+ksql < setup/createBusStream.sql
 ksql < setup/createGeoBins.sql
 ksql < setup/createAlertStream.sql
 ksql < setup/calibration.sql
--- setup/prepTopics.sh
+++ setup/prepTopics.sh
@@ -1,4 +1,4 @@
 kafka-topics --bootstrap-server localhost:9092 --create \
-  --topic ctaPrepped  --config retention.ms=-1
+  --topic busPrepped  --config retention.ms=-1
 kafka-topics --bootstrap-server localhost:9092 --create \
   --topic GEO_HEAT_MAP  --config retention.ms=-1
