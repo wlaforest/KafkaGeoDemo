87e944d24df8450aa4659fe9899ec56bbfaf2f05
Will LaForest <lacapaneus@gmail.com>
2020-09-22 00:17:28 -0400

    prepTopics no longer needs to precreate bus_preppd or GEO_HEAT_MAP

--- bin/prepTopics.sh
+++ bin/prepTopics.sh
@@ -1,8 +1,4 @@
 kafka-topics --bootstrap-server localhost:9092 --create \
   --topic bus_raw  --config retention.ms=-1
-kafka-topics --bootstrap-server localhost:9092 --create \
-  --topic bus_prepped  --config retention.ms=-1
-kafka-topics --bootstrap-server localhost:9092 --create \
-  --topic GEO_HEAT_MAP  --config retention.ms=-1
 
-tar -Oxzf data/wmata.csv.tgz | kafka-console-producer --bootstrap-server localhost:9092 --topic bus_raw > /dev/null
+bin/loadData.sh
