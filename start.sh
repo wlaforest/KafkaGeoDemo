ba221a11116d035f54ba833a81a3b1f8f78fdce6
Will LaForest <lacapaneus@gmail.com>
2020-12-07 15:12:46 -0500

    Adjusted the execution comment output

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 echo "JDK 11 or greater required"
-echo "Starting with config in conf/kesConfig.json"
-echo "To access go to http://localhost:8080 (or whatever port is configured)"
+echo "Confluent Platform 5.5 requirement"
 
 bin/prepTopics.sh
 
 bin/loadData.sh
 
 bin/prepKsql.sh
+
+echo "Starting with config in conf/kesConfig.json"
+echo "To access go to http://localhost:8080 (or whatever port is configured)"
 
 java -jar jars/KafkaEventService-1.0.1-fat.jar -conf conf/kesConfig.json
