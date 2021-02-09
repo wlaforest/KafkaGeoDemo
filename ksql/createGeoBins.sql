469d3240890a11da327c5e31efb65a4fbb292d31
wlaforest <lacapaneus@gmail.com>
2021-02-09 12:57:06 -0500

    Fixed for new CP 6.0 ksqlDB syntax changes

--- ksql/createGeoBins.sql
+++ ksql/createGeoBins.sql
@@ -1,7 +1,13 @@
 SET 'auto.offset.reset'='earliest';
 
 CREATE TABLE geo_heat_map AS
-  SELECT windowstart ws, windowend we, geohash, 1 unity, COUNT(*) total
+  SELECT
+    windowstart ws,
+    windowend we,
+    geohash rowkey,
+    AS_VALUE(geohash) as geohash,
+    1 unity,
+    COUNT(*) total
   FROM  bus
   WINDOW HOPPING (SIZE 30 SECONDS, ADVANCE BY 10 SECONDS)
   GROUP BY geohash
