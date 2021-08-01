330f8eed9cec9f7ffa9e068c487cbe8019a86218
wlaforest <lacapaneus@gmail.com>
2021-08-01 13:46:21 -0400

    tweaked connector scripts

--- bin/wmataSourceFileStreamConnector.sh
+++ bin/wmataSourceFileStreamConnector.sh
@@ -1,5 +1,6 @@
  # Uncomrpess the data file
  tar -xzf data/wmata.txt.tgz
+ mv wmata.txt /tmp
 
  # Spin up the source filsetream converter
  curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ \
@@ -8,7 +9,7 @@
         "config": {
           "connector.class": "FileStreamSource",
           "tasks.max": "1",
-          "file": "/tmp/wmata.csv",
+          "file": "/tmp/wmata.txt",
           "topic": "bus_raw",
           "key.converter":"org.apache.kafka.connect.storage.StringConverter",
           "value.converter":"org.apache.kafka.connect.storage.StringConverter"
