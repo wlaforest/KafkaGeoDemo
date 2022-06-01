46a429ad9388752b36a013995731460c11c4bf0e
William LaForest <lacapaneus@gmail.com>
2022-05-31 22:42:14 -0400

    added partitions to topic creation

--- bin/prepTopics.sh
+++ bin/prepTopics.sh
@@ -1,5 +1,5 @@
 kafka-topics --bootstrap-server localhost:9092 --create \
-  --topic bus_raw  --config retention.ms=-1
+  --topic bus_raw --partitions 1 --config retention.ms=-1
 
 kafka-topics --bootstrap-server localhost:9092 --create \
-  --topic bus_prepped  --config retention.ms=-1
+  --topic bus_prepped --partitions 1 --config retention.ms=-1
